6168db86059de0e06d2526e8a272fbba7b08c56f
efoejean <efoejean@gmail.com>
2021-03-20 12:06:26 -0500

    Display the emojis when "mouseover"

--- scripts.js
+++ scripts.js
@@ -39,6 +39,11 @@ const root = document.querySelector("#root");
 
 let divsHTML = "";
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 for (let i = 0; i < 1200; i += 1) {
   divsHTML += "<div>〰</div>\n";
 }
@@ -48,6 +53,7 @@ root.innerHTML = divsHTML;
 document.querySelectorAll("div:not(#root)").forEach((div) => {
   div.addEventListener("mouseover", ({ target }) => {
     const currentTarget = target;
-    currentTarget.innerText = "hi";
+    currentTarget.innerText =
+      emojis[getRandomIntInclusive(0, emojis.length - 1)];
   });
 });
