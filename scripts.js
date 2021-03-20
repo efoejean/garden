c46668406a05693595e4194a33c6f9bb78f08215
efoejean <efoejean@gmail.com>
2021-03-20 11:49:01 -0500

    Log message for all dis when "mouseover"

--- scripts.js
+++ scripts.js
@@ -1,14 +1,54 @@
-const emojis = ['🌵', '🌲', '🌳', '🌴', '🌱', '🌿', '☘️', '🍀', '🍃', '🌾', '🌷', '🌹', '🌺', '🌸', '🌼', '🌻', '🍄', '🍅', '🥕', '🍓','🐛', '🐝', '🐞', '🐌', '🐜', '🕷', '🦗', '🐍', '🦎', '🐀', '🐇', '🦋', '🦔', '🕊']
+const emojis = [
+  "🌵",
+  "🌲",
+  "🌳",
+  "🌴",
+  "🌱",
+  "🌿",
+  "☘️",
+  "🍀",
+  "🍃",
+  "🌾",
+  "🌷",
+  "🌹",
+  "🌺",
+  "🌸",
+  "🌼",
+  "🌻",
+  "🍄",
+  "🍅",
+  "🥕",
+  "🍓",
+  "🐛",
+  "🐝",
+  "🐞",
+  "🐌",
+  "🐜",
+  "🕷",
+  "🦗",
+  "🐍",
+  "🦎",
+  "🐀",
+  "🐇",
+  "🦋",
+  "🦔",
+  "🕊",
+];
 
 const squiggly = "〰";
 
-const root = document.querySelector("#root")
+const root = document.querySelector("#root");
 
 let divsHTML = "";
 
 for (let i = 0; i < 1200; i += 1) {
-    divsHTML +=  "<div>〰</div>\n";
+  divsHTML += "<div>〰</div>\n";
 }
 
 root.innerHTML = divsHTML;
 
+document.querySelectorAll("div:not(#root)").forEach((div) => {
+  div.addEventListener("mouseover", () => {
+    console.log("hi");
+  });
+});
